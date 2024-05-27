8f14a666527b2cd58220f5381d3e31f7463d3e0a
fabiosimplis <fabio.simplis@gmail.com>
2024-05-27 17:53:40 -0300

    ADD Entity and Test

---
+++ src/test/java/br/com/alura/codechella/domain/entities/usuario/UsuarioTest.java
@@ -0,0 +1,22 @@
+package br.com.alura.codechella.domain.entities.usuario;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+public class UsuarioTest {
+    @Test
+    public void naoDeveCadastrarUsuarioComCpfNoFormatoInvalido(){
+
+        Assertions.assertThrows(IllegalArgumentException.class,
+                () -> new Usuario("123456789-99", "Jacque", LocalDate.parse("1990-09-08"), "email@email.com"));
+    }
+
+    @Test
+    public void deveCadastrarUsuarioComCpfNoFOrmatoValido(){
+
+        Assertions.assertThrows(IllegalArgumentException.class,
+                () -> new Usuario("123456789-99", "Jacque", LocalDate.parse("1990-09-08"), "email@email.com"));
+    }
+}

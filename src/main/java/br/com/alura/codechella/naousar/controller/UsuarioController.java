32110c730db8f4feaa1fbd06dc25c5ed4d5160ad
fabiosimplis <fabio.simplis@gmail.com>
2024-05-27 17:53:18 -0300

    Correcting Package

--- src/main/java/br/com/alura/codechella/naousar/controller/UsuarioController.java
+++ src/main/java/br/com/alura/codechella/naousar/controller/UsuarioController.java
@@ -1,16 +1,12 @@
 package br.com.alura.codechella.naousar.controller;
 
 import br.com.alura.codechella.naousar.model.Usuario;
-import br.com.alura.codechella.service.UsuarioService;
+import br.com.alura.codechella.naousar.service.UsuarioService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.List;

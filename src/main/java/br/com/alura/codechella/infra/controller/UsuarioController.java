84e0e719af92fc3ccd4e07bedb5f094a18575c62
fabiosimplis <fabio.simplis@gmail.com>
2024-05-28 13:23:38 -0300

    Implementando Listar Usuários

--- src/main/java/br/com/alura/codechella/infra/controller/UsuarioController.java
+++ src/main/java/br/com/alura/codechella/infra/controller/UsuarioController.java
@@ -1,20 +1,22 @@
 package br.com.alura.codechella.infra.controller;
 
 import br.com.alura.codechella.application.usecases.CriarUsuario;
+import br.com.alura.codechella.application.usecases.ListarUsuarios;
 import br.com.alura.codechella.domain.entities.usuario.Usuario;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/usuarios")
 public class UsuarioController {
 
     private final CriarUsuario criarUsuario;
+    private final ListarUsuarios listarUsuarios;
 
-    public UsuarioController(CriarUsuario criarUsuario) {
+    public UsuarioController(CriarUsuario criarUsuario, ListarUsuarios listarUsuarios) {
         this.criarUsuario = criarUsuario;
+        this.listarUsuarios = listarUsuarios;
     }
 
     @PostMapping
@@ -23,5 +25,13 @@ public class UsuarioController {
 
         return new UsuarioDto(usuarioCriado.getCpf(), usuarioCriado.getNome(), usuarioCriado.getNascimento(), usuarioCriado.getEmail());
 
+    }
+
+    @GetMapping
+    public List<UsuarioDto> listarUsuarios(){
+
+        return listarUsuarios.obterTodosUsuarios().stream()
+                .map(u -> new UsuarioDto(u.getCpf(), u.getNome(), u.getNascimento(), u.getEmail()))
+                .toList();
     }
 }
